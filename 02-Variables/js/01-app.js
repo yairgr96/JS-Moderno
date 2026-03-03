5891e0f0a001eb5a84cb649f024fefaf496f4113
yairgr96 <yairgr96@gmail.com>
2026-03-03 08:56:41 -0600

    Añandiendo practicas String y variables

---
+++ 02-Variables/js/01-app.js
@@ -0,0 +1,13 @@
+var teclado;
+console.log(teclado);
+var teclado = "Logitech G Pro";
+
+console.log(teclado);
+
+let nombre = "Yair";
+console.log(nombre);
+
+let nombre = "Yair gamer";
+console.log(nombre);
+
+const PI;
---
+++ 03-Strings/js/01-app.js
@@ -0,0 +1,12 @@
+const correo = " JUAN.PABLO@CORREO.COM ";
+
+console.log(correo.trimStart() + " " + correo.trimEnd()); // Elimina los espacios al inicio y al final
+
+console.log(correo.trim()); // Elimina los espacios al inicio y al final
+
+console.log(correo.toLowerCase()); // Convierte a minúsculas
+
+correo.toUpperCase(); // Convierte a mayúsculas
+correo.trim(); // Elimina los espacios al inicio y al final
+
+console.log(correo.length); // El valor de correo no ha cambiado, sigue teniendo los espacios y mayúsculas
